c295fd54d659900ea25871d4375efe677ac548aa
Chaiwat Shuetrakoonpaiboon <iwatth@gmail.com>
2015-11-18 17:31:09 +0700

    Fix compile error on cross compile

--- authtype_stub.go
+++ authtype_stub.go
@@ -2,4 +2,21 @@
 
 package seckeychain
 
+// See https://developer.apple.com/library/mac/documentation/Security/Reference/keychainservices/index.html for the APIs used below.
+
+// Also see https://developer.apple.com/library/ios/documentation/Security/Conceptual/keychainServConcepts/01introduction/introduction.html .
+
 type AuthenticationType int
+
+// From http://www.opensource.apple.com/source/libsecurity_keychain/libsecurity_keychain-34101/lib/SecKeychain.h
+const (
+	AuthenticationTypeNTLM = iota
+	AuthenticationTypeMSN
+	AuthenticationTypeDPA
+	AuthenticationTypeRPA
+	AuthenticationTypeHTTPBasic
+	AuthenticationTypeHTTPDigest
+	AuthenticationTypeHTMLForm
+	AuthenticationTypeDefault
+	AuthenticationTypeAny
+)
--- protocol_stub.go
+++ protocol_stub.go
@@ -2,4 +2,47 @@
 
 package seckeychain
 
+// See https://developer.apple.com/library/mac/documentation/Security/Reference/keychainservices/index.html for the APIs used below.
+
+// Also see https://developer.apple.com/library/ios/documentation/Security/Conceptual/keychainServConcepts/01introduction/introduction.html .
+
 type ProtocolType int
+
+// From http://www.opensource.apple.com/source/libsecurity_
+const (
+	ProtocolTypeFTP = iota
+	ProtocolTypeFTPAccount
+	ProtocolTypeHTTP
+	ProtocolTypeIRC
+	ProtocolTypeNNTP
+	ProtocolTypePOP3
+	ProtocolTypeSMTP
+	ProtocolTypeSOCKS
+	ProtocolTypeIMAP
+	ProtocolTypeLDAP
+	ProtocolTypeAppleTal
+	ProtocolTypeAFP
+	ProtocolTypeTelnet
+	ProtocolTypeSSH
+	ProtocolTypeFTPS
+	ProtocolTypeHTTPS
+	ProtocolTypeHTTPProxy
+	ProtocolTypeHTTPSProxy
+	ProtocolTypeFTPProxy
+	ProtocolTypeCIFS
+	ProtocolTypeSMB
+	ProtocolTypeRTSP
+	ProtocolTypeRTSPProxy
+	ProtocolTypeDAAP
+	ProtocolTypeEPPC
+	ProtocolTypeIPP
+	ProtocolTypeNNTPS
+	ProtocolTypeLDAPS
+	ProtocolTypeTelnetS
+	ProtocolTypeIMAPS
+	ProtocolTypeIRCS
+	ProtocolTypePOP3S
+	ProtocolTypeCVSpserver
+	ProtocolTypeSVN
+	ProtocolTypeAny
+)
